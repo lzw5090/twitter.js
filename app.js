d14bcb2deaf56aec738f92696cd7610f40d19f59
Asmaa H Abbas <abbas.asmaa@gmail.com>
2017-08-01 11:09:21 -0400

    logged middleware

---
+++ app.js
@@ -0,0 +1,32 @@
+// const express = require('express')
+const app = require("express")();
+const volleyball = require('volleyball');
+
+app.use('/news', function (req, res, next) {
+    // do your logging here
+    // call `next`, or else your app will be a black hole — receiving requests but never properly responding
+    // console.log(Object.keys(req));
+    console.log(req.method, req.url, res.statusCode);
+    next();
+})
+
+app.use('/special', function(req, res, next){
+  console.log('You reached the special area');
+  next();
+})
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.get('/news', function(req, res){
+  res.send('This is the news page!');
+})
+
+app.get('/special', function(req, res, next){
+  res.send('Heyyy, you reached the special area');
+});
+
+app.listen(3000, function () {
+  console.log('Server Listening');
+})
